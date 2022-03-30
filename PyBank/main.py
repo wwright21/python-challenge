94488a682efc9d7c56ac8585f33132106423f165
wwright21 <williamcwrightjr@gmail.com>
2022-03-29 22:39:00 -0400

    Found all variables, now just need the final print statement.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,23 +11,56 @@ csvpath = os.path.join("Resources","budget_data.csv")
 
 # read in the csv file, specify encoding as utf
 with open(csvpath,encoding='utf') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
+    csvreader = csv.DictReader(csvfile, delimiter = ',')
 
-    # Store the header row as a variable
-    csv_header = next(csvfile)
+    # # Store the header row as a variable
+    # csv_header = next(csvfile)
     
-    data_dict = {rows[0]:int(rows[1]) for rows in csvreader}
-    
-    totalMonths = len(data_dict)
-    total = sum(data_dict.values())
-    # averageChange = 
-    greatestInc = max(data_dict, key=data_dict.get)
+    monthList = []
+    profitList = []
 
-print(f"""Financial Analysis
-------------------------------
-Total Months: {totalMonths}
-Total: ${total}
-Average Change: 
-Greatest Increase in Profits: {greatestInc}
-Greatest Decrease in Profits: 
-""")
+    for col in csvreader:
+        monthList.append(col['Date'])
+        profitList.append(int(col['Profit/Losses']))
+
+# This will find the total number of months (bullet point #1)
+totalMonths = len(monthList)
+print(totalMonths)
+
+# This will find the total profit/loss over the entire period (bullet point #2)
+totalProfit = sum(profitList)
+print(totalProfit)
+
+# This will find the month-over-month changes and then the
+# average of those changes (bullet point #3)
+deltaList = []
+for x, y in zip(profitList[0::], profitList[1::]):
+    deltaList.append(y-x)
+avgChange = sum(deltaList)/len(deltaList)
+print(avgChange)
+
+# Using the max() function, this code will find the greatest increase in month-over-
+# month profit and return the corresponding month when this occurred (bullet point #4)
+maxIncrease = max(deltaList)
+monthmaxIncrease = monthList[deltaList.index(maxIncrease)+1]
+print(maxIncrease)
+print(monthmaxIncrease)
+
+# Using the min() function, this code will find the greatest decrease in month-over-
+# month profit and return the corresponding month when this occurred (bullet point #5)
+maxDecrease = min(deltaList)
+monthmaxDecrease = monthList[deltaList.index(maxDecrease)+1]
+print(maxDecrease)
+print(monthmaxDecrease)
+
+
+
+# print(f"""Financial Analysis
+# ------------------------------
+# Total Months: {totalMonths}
+# Total: ${total}
+# Average Change: 
+# Greatest Increase in Profits: {greatestInc}
+# Greatest Decrease in Profits: 
+# """)
+
