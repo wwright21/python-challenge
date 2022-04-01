e2b73d5f9dc81ec325ce86aa71bcaca5b95a2a7b
wwright21 <williamcwrightjr@gmail.com>
2022-03-31 23:20:25 -0400

    Got the script fully running

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -9,35 +9,55 @@ from collections import defaultdict
 # next, create variable to the path where the csv lives
 csvpath = os.path.join("Resources","election_data.csv")
 
-# read in the csv file, specify encoding as utf, and convert to a dictionary
-# Note that the code reads in the first and third column of the csv file,
-# as we are not particularly concerned with the second column ('county')
+# read in the csv file using the DictReader class. We create an empty
+# list to store all the values in the "Candidate" column
 with open(csvpath,encoding='utf') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',', skipinitialspace=True)
-    header = next(csvreader)
-    electionDict = {rows[0]:rows[2] for rows in csvreader}
+    csvreader = csv.DictReader(csvfile, delimiter = ',')
+    votesList = []
+    for col in csvreader:
+        votesList.append(col["Candidate"])
 
-# Now invert the dictionary mapping so we get 1 candidate and then every unique
-# ID that voted for him / her
-invElectionDict = defaultdict(list)
-for key, value in electionDict.items():
-    invElectionDict[value].append(key)
-
-# create another dictionary to print the length of values for each key (aka politician)
-# by using a dictionary comprehension. This dictionary will contain everything
-# we need to solve this challenge!
-length_ElectionDict = {key: len(value) for key, value in invElectionDict.items()}
+# To make things run a little more quickly, we use the set() function to
+# get a list of the unique values in the "Candidates" column. We then
+# use dictionary comprehension to create a new dictionary with all the 
+# votes tallied up by candidate 
+setList = list(set(votesList))
+votesDict = {i:votesList.count(i) for i in setList}
 
 # Find the total votes
-totalVotes = sum(length_ElectionDict.values())
+totalVotes = sum(votesDict.values())
 
 # The votes by candidate is already stored as the value in our length_ElectionDict. So,
 # let's find the percentage breakdown by candidate
-CCSperc = round(length_ElectionDict['Charles Casper Stockham'] / totalVotes * 100,3)
-DDperc = round(length_ElectionDict['Diana DeGette'] / totalVotes * 100,3)
-RADperc = round(length_ElectionDict['Raymon Anthony Doane'] / totalVotes * 100,3)
+CCSperc = round(votesDict['Charles Casper Stockham'] / totalVotes * 100,3)
+DDperc = round(votesDict['Diana DeGette'] / totalVotes * 100,3)
+RADperc = round(votesDict['Raymon Anthony Doane'] / totalVotes * 100,3)
 
 # Find the winning politician by first finding the max value of the 
-# length_ElectionDict dictionary 
-winner = list(length_ElectionDict.keys())[list(length_ElectionDict.values()).index(max(length_ElectionDict.values()))]
-print(winner)
+# votesDict dictionary 
+winner = list(votesDict.keys())[list(votesDict.values()).index(max(votesDict.values()))]
+
+# Here is a list of text lines we'll loop through to write the results
+# Note that we format any numbers with commas as thousands separators
+textLines = ["Election Results","-----------------------------------------",
+            f"Total Votes: {totalVotes:,}","-----------------------------------------",
+            f"Charles Casper Stockham: {CCSperc}% ({votesDict['Charles Casper Stockham']:,})",
+            f"Diana DeGette: {DDperc}% ({votesDict['Diana DeGette']:,})",
+            f"Raymon Anthony Doane: {RADperc}% ({votesDict['Raymon Anthony Doane']:,})",
+            "-----------------------------------------",f"Winner: {winner}",
+            "-----------------------------------------"           
+]
+
+# Now that we have all the variables we'll need in our print statement,
+# let's specify a path to write this to a .txt file
+export_path = os.path.join("analysis","results.txt")
+
+# open file in write mode
+with open(export_path,'w') as results:
+    for x in textLines:
+        results.write(x)
+        results.write("\n")
+
+# Print results to the gitbash terminal
+for x in textLines:
+    print(x)
